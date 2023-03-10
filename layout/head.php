11d5194f31fd33c4eb1c4f429b045a638e60168c
Carlos Tlelo Barcena <carlos.tielo@coppel.com>
2023-03-09 19:48:00 -0700

    Integracion de google analitycs

--- layout/head.php
+++ layout/head.php
@@ -55,9 +55,32 @@
 			href="<?php echo $favicon->href;?>" size="<?php echo $favicon->size; ?>" />
 		<?php endif; ?>
 	<?php endforeach; ?>
+
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NRWTXVH');</script>
+<!-- End Google Tag Manager -->
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VPFXB1RG97"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VPFXB1RG97');
+</script>
 	
 </head>
 <body <?php if( $class_page ) echo " class='".$class_page."'"; ?>>
 
 </head>
 <body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NRWTXVH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
